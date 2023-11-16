216f7581e20d0601d439b4e2d4f252629f054466
SeungWoo <tmddn0849@gmail.com>
2023-11-16 22:30:38 +0900

    init

---
+++ src/main/java/com/auction/WebAuction/WebAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.auction.WebAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auction/WebAuction/WebAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.WebAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
